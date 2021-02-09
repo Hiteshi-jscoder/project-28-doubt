5540e071ecd02f70127af9d74f8079e71d2fa6f2
Hiteshi Singla <72081322+Hiteshi-jscoder@users.noreply.github.com>
2021-02-09 22:50:54 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,25 @@
+class Stone{
+constructor(x,y){
+    var options = {
+        'restitution':0,
+        'friction':1.0,
+        'density':1.2,
+        isStatic : true
+    }
+    this.body = Bodies.rectangle(x, y, 50, 50)
+    this.width= 50;
+    this.height= 50;
+    this.image= loadImage('Plucking mangoes/stone.png');
+    World.add(world,this.body);
+}
+
+ display(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+}
+}
